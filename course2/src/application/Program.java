340b46929c72ba5d7ec8f617c1edbe3a776a3374
unknown <arthurauersvald@gmail.com>
2025-04-09 16:23:22 -0300

    atualizacao

--- course2/src/application/Program.java
+++ course2/src/application/Program.java
@@ -14,14 +14,33 @@ public class Program {
 		
 		Product product = new Product();
 		System.out.println("Enter product data: ");
-		System.out.println("Name: ");
+		System.out.print("Name: ");
 		product.name = sc.nextLine();
 		System.out.print("Price: ");
 		product.price = sc.nextDouble();
-		System.out.println("Quantity in stock: ");
+		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
+		product.toString();
 		
+		System.out.println();
+		System.out.println("Product data: " + product.toString());
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed in stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.print("Updated data: " + product);
 		
 		sc.close();
 	}
